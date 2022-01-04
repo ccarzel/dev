b6c78fc0bedd592561a23d61f9a6649ec882da7a
ccarzel <czelada@dbperu.com>
2022-01-04 23:43:57 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,25 @@
+#-*-coding: utf-8-*-
+{
+    'name':'Odoo Academy',
+    'summary':"""Academy app to manage tarining""",
+    'description': """
+    Academy mobile to mamange training:
+    -couses
+    -session
+    - Attenders
+        """,
+    'author':'odoo'.
+    'website':'httos://www.odoo.com',
+    'categpry':'trainign',
+    'version':'0,1',
+    
+    'depends':['base'],
+    'data':[
+        
+    ],
+    
+    'demo':[
+        
+    ],
+    
+}
